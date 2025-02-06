2e32f9f49467730dad266f4b6a22b4497a38fbe4
eriba-ab <eribainc@gmail.com>
2025-02-06 16:46:11 +0100

    fixed database conflict

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    "default": {
+        "ENGINE": "django.db.backends.dummy",  # Dummy backend for no database
+    }
 }
 
 
